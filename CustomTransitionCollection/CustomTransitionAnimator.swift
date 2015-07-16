d9aa6e7a7aa00096adedbfe15960931aebddbbd0
Masamichi Ueta <masamichi1023@gmail.com>
2015-07-16 18:01:26 +0900

    add UIVisualEffectView

---
+++ CustomTransitionCollection/CustomTransitionAnimator.swift
@@ -0,0 +1,25 @@
+//
+//  CustomTransitionAnimator.swift
+//  CustomTransitionCollection
+//
+//  Created by UetaMasamichi on 2015/07/16.
+//  Copyright © 2015年 Masamichi Ueta. All rights reserved.
+//
+
+import UIKit
+
+class CustomTransitionAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+        return 0.5
+    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        
+        
+        
+    }
+    
+    
+    
+}
